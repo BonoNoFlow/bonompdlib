6061761431fd7310cacc0bd38023d3919a5f171e
hendriknieuwenhuis <hendrik.nieuwenhuis@gmail.com>
2016-05-31 04:07:35 +0200

    added changelistener class

--- src/main/java/com/bono/api/Config.java
+++ src/main/java/com/bono/api/Config.java
@@ -11,7 +11,15 @@ public class Config {
     private String host;
     private int port;
 
-    public Config() {}
+    /*
+    TODO werken met Properties.
+
+     */
+    private Properties properties;
+
+    public Config() {
+        properties = new Properties();
+    }
 
     public Config(String host, int port) {
         this.host = host;
@@ -44,11 +52,43 @@ public class Config {
 
     }
 
+    public Properties loadProperties() throws Exception {
+        Properties properties = new Properties();
+        InputStream inputStream = null;
+
+        // First try loading from the current directory.
+        try {
+            File file = new File("server.properties");
+            inputStream = new FileInputStream(file);
+        } catch (Exception e) {
+            inputStream = null;
+        }
+
+        /* here's where the exception throw comes from. */
+        if (inputStream == null) {
+            // try loading from classpath.
+            inputStream = getClass().getResourceAsStream("server.properties");
+        }
+        // try loading properties from the file (if found)
+        properties.load(inputStream);
+        return properties;
+    }
+
     public void saveParamChanges() throws Exception {
         try {
             Properties properties = new Properties();
             properties.setProperty("HostAddress", host);
             properties.setProperty("Port", Integer.toString(port));
+            File file = new File("server.properties");
+            OutputStream out = new FileOutputStream(file);
+            properties.store(out, "properties");
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    public void saveParamChanges(Properties properties) throws Exception {
+        try {
             File file = new File("server.properties");
             OutputStream out = new FileOutputStream(file);
             properties.store(out, "properties");
