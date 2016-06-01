3deb16ebfd64dd543e2f86d64117f1b170813a1e
hendriknieuwenhuis <hendrik.nieuwenhuis@gmail.com>
2016-06-01 03:48:58 +0200

    changed config!!

--- src/main/java/com/bono/api/Config.java
+++ src/main/java/com/bono/api/Config.java
@@ -6,53 +6,16 @@ import java.util.Properties;
 /**
  * Created by hendriknieuwenhuis on 17/02/16.
  */
-@Deprecated
-public class Config {
+abstract class Config {
 
     public static final String CONFIG_FILE = "server.properties";
+    public static final String HOST = "host";
+    public static final String PORT = "port";
 
-    private String host;
-    private int port;
-
-    /*
-    TODO werken met Properties.
-
-     */
     protected Properties properties;
 
     public Config() {
         properties = new Properties();
-    }
-
-    public Config(String host, int port) {
-        this.host = host;
-        this.port = port;
-    }
-
-
-    public void loadParams() throws Exception {
-        Properties properties = new Properties();
-        InputStream inputStream = null;
-
-        // First try loading from the current directory.
-        try {
-            File file = new File(Config.CONFIG_FILE);
-            inputStream = new FileInputStream(file);
-        } catch (Exception e) {
-            inputStream = null;
-        }
-
-        /* here's where the exception throw comes from. */
-        if (inputStream == null) {
-             // try loading from classpath.
-            inputStream = getClass().getResourceAsStream(Config.CONFIG_FILE);
-        }
-        // try loading properties from the file (if found)
-        properties.load(inputStream);
-
-        host = properties.getProperty("HostAddress");
-        port = new Integer(properties.getProperty("Port"));
-
     }
 
     public void loadConfig() throws Exception {
@@ -76,19 +39,6 @@ public class Config {
         properties.load(inputStream);
     }
 
-    public void saveParamChanges() throws Exception {
-        try {
-            Properties properties = new Properties();
-            properties.setProperty("HostAddress", host);
-            properties.setProperty("Port", Integer.toString(port));
-            File file = new File(Config.CONFIG_FILE);
-            OutputStream out = new FileOutputStream(file);
-            properties.store(out, "properties");
-        } catch (Exception e) {
-            throw e;
-        }
-    }
-
     public void saveConfig() throws Exception {
         try {
             File file = new File(Config.CONFIG_FILE);
@@ -99,23 +49,23 @@ public class Config {
         }
     }
 
-    public void setProperty(String key, String property) {
+    protected void setProperty(String key, String property) {
         properties.setProperty(key, property);
     }
 
     public String getHost() {
-        return host;
+        return properties.getProperty(Config.HOST);
     }
 
     public void setHost(String host) {
-        this.host = host;
+        properties.setProperty(Config.HOST, host);
     }
 
     public int getPort() {
-        return port;
+        return Integer.parseInt(properties.getProperty(Config.PORT));
     }
 
-    public void setPort(int port) {
-        this.port = port;
+    public void setPort(String port) {
+        properties.setProperty(Config.PORT, port);
     }
 }
